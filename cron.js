5d1a2f60ad98a956a1c291961795ce41b40f3e95
Stuart Pomeroy <rushmead@gmail.com>
2022-02-01 21:54:59 +0000

    fix cronjob

--- cron.js
+++ cron.js
@@ -2,12 +2,13 @@ const https = require('https');
 const path = require('path');
 const fs = require('fs');
 const getCourses = require('./getAllCourses.js');
+const DATA_DIR = process.env.DATA_DIR || "data";
 async function run(){
     let courses = [];
-    if(!fs.existsSync(path.join(__dirname, 'courses.json'))){
-        fs.writeFileSync(path.join(__dirname, 'courses.json'), JSON.stringify(courses));
+    if(!fs.existsSync(path.join(__dirname, DATA_DIR, 'courses.json'))){
+        fs.writeFileSync(path.join(__dirname, DATA_DIR, 'courses.json'), JSON.stringify(courses));
     } else {
-        courses = JSON.parse(fs.readFileSync(path.join(__dirname, 'courses.json')));
+        courses = JSON.parse(fs.readFileSync(path.join(DATA_DIR, 'courses.json')));
     }
 console.log("running...");
     await getCourses(courses);
